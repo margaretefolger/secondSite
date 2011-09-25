18332cabccc8d84470b83d20b7733131ed859ac0
Margarete Folger <reinprechtsdorf@hotmail.com>
2011-09-25 23:57:42 +0200

    SecondSite_01

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecondSite::Application.config.secret_token = 'e3688b19d8a12025a98ee7473febff704370de131879c2d04a8d336984fba05ded6c4ed021acda8f64066244c630d95706fa0aaff0a9f1a106f62a0fbcea5373'
