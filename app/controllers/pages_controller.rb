3dfc35540f379757b524e2e87911edf9c0722e0c
Margarete Folger <reinprechtsdorf@hotmail.com>
2011-09-27 00:35:13 +0200

    secondSite_05_staticPages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
   def rootpage
+    @title = "This is the root page, where everything starts"
   end
 
   def talktome
+    @title = "Talk to me"
   end
 
   def whoami
+    @title = "who am I"
   end
 
 end
