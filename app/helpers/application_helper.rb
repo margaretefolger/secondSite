00a582b3a9a7dd89f3c3f724e8ec6b6e3373e085
Margarete Folger <reinprechtsdorf@hotmail.com>
2011-09-27 23:25:47 +0200

    secondSite_02_css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Return a title on a per-page basis.
+  def helpertitle
+    base_title = "Do it again"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
