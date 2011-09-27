c5f97d241c6d2f118e4dae5ed72a312c07711166
Margarete Folger <reinprechtsdorf@hotmail.com>
2011-09-28 00:54:17 +0200

    secondSite_07_partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 SecondSite::Application.routes.draw do
-  get "pages/rootpage"
+#  get "pages/rootpage"
+#  get "pages/talktome"
+#  get "pages/whoami"
 
-  get "pages/talktome"
 
-  get "pages/whoami"
+  root :to => 'pages#rootpage'
+  match '/rootpage', :to => 'pages#rootpage'
+  match '/talktome',   :to => 'pages#talktome'
+  match '/whoami',    :to => 'pages#whoami'
+
 end
